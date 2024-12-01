b48bb932863f18f90da28473490d22475d1c81f9
Dart Vader <adigam80@gmail.com>
2024-12-01 09:51:30 +0300

    Add files via upload

--- modui_1_5.py
+++ modui_1_5.py
@@ -1,31 +1,11 @@
-immutable_var = (1, 2, 'a', 'b')
-print(immutable_var)
-Mutable_list = [1, 2, 'a', 'b', 'Modified']
-print(Mutable_list)
+from pkgutil import extend_path
 
-food = ["appele", "coconate", "banana"]
-print(food)
-print(food[0])
-food [2] = "ice cream"
-print(food)
-food.append(123)
-print(food)
-food.extend("banana")
-print(food)
-food.extend(["banana", 321])
-print(food)
-food.remove("coconate")
-print(food)
-tuple_ = 1, 2, 3, 4
-tuple_2 = 1, 2, 3, 4
-tuple_3 = ([1, 2, 3, 4])
-print(tuple_)
-print(tuple_2)
-print(tuple_3)
-print(tuple_3[1])
-tuple_ = ([1, 2,], 3, 4)
-print(tuple_)
-tuple_[0] [1] = 3
-print(tuple_)
-tuple_ = (1, 2, 3) * 2
-print(tuple_)
+immutable_var = 1, 2, 'a', 'b'
+print((immutable_var) *2)
+Mutable_list = [1, 2, 'a', 'b', 'Modified'] * 2
+Mutable_list [2] = 3
+Mutable_list [3] = 4
+Mutable_list [4] = 5
+Mutable_list.remove('Modified')
+Mutable_list.extend('Fack')
+print(Mutable_list)
